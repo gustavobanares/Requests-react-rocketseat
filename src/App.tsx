59694edcbe8c5833ba738ca18bbb35ece99d7d6a
Gustavo Bañares <gustavobbacco@gmail.com>
2025-11-25 18:14:53 -0300

    feat: implement UsersList component and useUsers hook for user data fetching

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
 import UserInfo from "./components/user-info";
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
 import UserNewForm from "./components/user-new-form";
+import UsersList from "./components/users-list";
+
+const queryClient = new QueryClient()
 
 
 function App() {
 
 
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
    <UserInfo />
    <hr />
    <UserNewForm />
-  </>
+   <hr />
+   <UsersList />
+  </QueryClientProvider>
   )
 
 }
